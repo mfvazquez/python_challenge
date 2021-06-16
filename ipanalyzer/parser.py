33f5a56461d4faffe465cb804483d5b3e7cb838e
Matias Vazquez <matias.vazquez@robintests.com>
2021-06-16 15:57:15 -0300

    Adding a parser  to search  IP addresses. #1

---
+++ ipanalyzer/parser.py
@@ -0,0 +1,24 @@
+import re
+
+
+class IPFinder:
+
+    def __init__(self):
+        self.pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+
+    def find(self, filename):
+        """ Returns a set of IP addresses found in the given file.
+
+        Args:
+            filename (string): The file name to search for IP addresses.
+
+        Returns:
+            set: A set of IP addresses.
+        """
+        ips = set()
+        with open(filename) as file:
+            line = file.readline()
+            while line:
+                ips.update(self.pattern.findall(line))
+                line = file.readline()
+        return ips
